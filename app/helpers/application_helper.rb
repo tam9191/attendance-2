028e688a2e93cccaab1806897d057a6ce54892e1
masa <m.t.habite@gmail.com>
2019-08-21 07:45:32 +0000

    Finish top-page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  #　ページごとにタイトルを返す
+  
+  def full_title(page_name = "") #メソッドと引数の定義
+    base_title = "AttendanceApp" # 基本となるアプリケーション名
+    if page_name.empty? # もしページ名が空だったら
+      base_title # 基本となるアプリケーション名だけを返す
+    else # ページ名が入っていたら
+      page_name + "|" + base_title # ページ名+ | + 基本となるアプリケーション名
+    end
+  end
 end
